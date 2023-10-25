51044a0f80280e51aa8c6d845f0652db70effbc4
Binh Nguyen <103493942+binhnguyen5658@users.noreply.github.com>
2023-10-25 21:01:40 +0700

    Update cronjob.py

--- cronjob.py
+++ cronjob.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from time import sleep
 
 # create a new instance of the Firefox driver
-driver = webdriver.Chrome()
+driver = webdriver.Firefox()
 driver.implicitly_wait(10)
 
 # navigate to the login page
