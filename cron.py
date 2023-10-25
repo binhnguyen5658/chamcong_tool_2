da49e54f01ce758144d5becece276eb9a5142b0d
Binh Nguyen <103493942+binhnguyen5658@users.noreply.github.com>
2023-10-25 17:06:29 +0700

    Add files via upload

---
+++ cron.py
@@ -0,0 +1,29 @@
+# import necessary modules
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+# create a new instance of the Firefox driver
+driver = webdriver.Chrome()
+driver.implicitly_wait(10)
+
+# navigate to the login page
+driver.get("https://nncchamcongtool.streamlit.app")
+# driver.get("https://share.streamlit.io/")
+
+try:
+    element = driver.find_element(By.XPATH, '//button[contains(text(),"app back up")]')
+    # element = driver.find_element(By.XPATH, '//button[contains(text(),"with Google")]')
+except:
+    print('still awake')
+    pass
+else:
+    print('awake app')
+    element.click()
+
+sleep(10)
+
+driver.close()
+
+
+
